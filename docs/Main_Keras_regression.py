217cfaf92d314ef466fe954e8af967458589f7c0
Chih-Sheng (Tommy) Huang <chih.sheng.huang821@gmail.com>
2017-08-17 16:01:11 +0800

    Update Main_Keras_regression.py

--- docs/Main_Keras_regression.py
+++ docs/Main_Keras_regression.py
@@ -12,21 +12,30 @@ Y = 3 * X + 2 + np.random.normal(0, 0.05, (100, ))
 
 X_train, Y_train = X[:60], Y[:60]     # train 前 160 data points
 X_test, Y_test = X[60:], Y[60:]       # test 后 40 data points
-
+plt.subplot(211)
+plt.scatter(X_train, Y_train)
+plt.xlabel('X_train')
+plt.ylabel('Y_train')
+plt.subplot(212)
+plt.scatter(X_test, Y_test)
+plt.xlabel('X_test')
+plt.ylabel('Y_test')
+plt.show()
+                
  # build a neural network from the 1st layer to the last layer
 model = Sequential()
-model.add(Dense(output_dim=10, input_dim=1))
-model.add(Dense(output_dim=1, input_dim=10, activation='linear'))
+model.add(Dense(output_dim=100, input_dim=1, activation='relu'))
+model.add(Dense(output_dim=1, input_dim=100))
 model.compile(loss='mse', optimizer='sgd') # choose loss function and optimizing method
 
 print('Training -----------')
-for step in range(1001):
+for step in range(10000):
     cost = model.train_on_batch(X_train, Y_train)
-    if step % 10 == 0:
+    if step % 1000 == 0:
         print('train cost: ', cost)
         
 print('\nTesting ------------')
-cost = model.evaluate(X_test, Y_test, batch_size=40)
+cost = model.evaluate(X_test, Y_test)
 print('test cost:', cost)
 W, b = model.layers[0].get_weights()
 print('Weights=', W, '\nbiases=', b)
